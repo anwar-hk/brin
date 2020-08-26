4f9776bed4a447c1ac1dbb1bbf75089e9b78bc64
Inaya <anwar@anwars-MacBook-Pro.local>
2020-08-26 19:42:15 +0530

    theam styles added

---
+++ resources/views/frontend/home/nav.blade.php
@@ -0,0 +1,86 @@
+<!-- Main Header-->
+<header class="main-header">
+    <!-- Main box -->
+    <div class="main-box">
+        <div class="menu-box">
+            <div class="logo"><a href="index.html"><img src="images/logo.png" alt="" title="" srcset="images/logo-white-big.png 2x"></a></div>
+            <!--Nav Box-->
+            <div class="nav-outer">
+                <!-- Main Menu -->
+                <nav class="main-menu navbar-expand-md navbar-light">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navigation clearfix">
+                            <li>
+                                <a href="index.html"><span>Home</span></a>
+                            </li>
+                            <li class="dropdown">
+                                <a><span>Services</span></a>
+                                <ul>
+                                    <li><a href="portfolio-masonry.html">Home Services</a></li>
+                                    <li><a href="portfolio-two-col-sidebar.html">Office Services</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact"><span>About</span> </a></li>
+                            <li><a href="contact"><span>News</span> </a></li>
+                            <li><a href="contact"><span>Contacts</span> </a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-- Sticky Header  -->
+    <div class="sticky-header">
+        <div class="outer-box">
+            <!--Logo-->
+            <div class="logo">
+                <a href="#" title="Sticky Logo"><img src="images/sticky-logo.png" alt="Sticky Logo"></a>
+            </div>
+
+            <!--Nav Outer-->
+            <div class="nav-outer">
+                <!-- Main Menu -->
+                <nav class="main-menu">
+                    <!--Keep This Empty / Menu will come through Javascript-->
+                </nav><!-- Main Menu End-->
+            </div>
+        </div>
+    </div><!-- End Sticky Menu -->
+
+    <!-- Mobile Header -->
+    <div class="mobile-header">
+        <div class="logo"><a href="index.html"><img src="images/logo.png" alt="" title="" srcset="images/logo-white-big.png 2x"></a></div>
+
+        <!--Nav Box-->
+        <div class="nav-outer clearfix">
+            <!--Keep This Empty / Menu will come through Javascript-->
+        </div>
+    </div>
+
+    <!-- Mobile Menu  -->
+    <div class="mobile-menu">            
+        <nav class="menu-box">
+            <div class="nav-logo"><a href="index.html"><img src="images/logo.png" alt="" title="" srcset="images/logo-white-big.png 2x"></a></div> 
+            <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+        </nav>
+    </div><!-- End Mobile Menu -->
+
+    <!-- Header Search -->
+    <div class="search-popup">
+        <span class="search-back-drop"></span>
+        
+        <div class="search-inner">
+            <button class="close-search"><span class="fa fa-times"></span></button>
+            <form method="post" action="blog-showcase.html">
+                <div class="form-group">
+                    <input type="search" name="search-field" value="" placeholder="Search..." required="">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- End Header Search -->
+
+</header>
+<!--End Main Header -->
---
+++ resources/views/layouts/footer_link.blade.php
@@ -0,0 +1,104 @@
+
+    <!-- Main Footer -->
+    <footer class="main-footer" style="background-image: url(https://via.placeholder.com/1920x600);">
+        <div class="auto-container">
+            <!--Widgets Section-->
+            <div class="widgets-section">
+                <div class="row">
+                    <!--Footer Column-->
+                    <div class="footer-column col-lg-4 col-md-6 col-sm-12">
+                        <div class="footer-widget about-widget">
+                            <h4 class="widget-title">INGENIOUS</h4>
+                            <div class="widget-content">
+                                <div class="text">Etiam id scelerisque est, vel scelerisque elit. Etiam vitae consequat dui. Suspendisse in porta felis, a varius elit. Sed nec fringilla mauris. Pellentesque habitant morbi.</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="footer-column col-lg-4 col-md-6 col-sm-12">
+                        <div class="footer-widget recent-posts">
+                            <h4 class="widget-title">LATEST POSTS</h4>
+                             <!--Footer Column-->
+                            <div class="widget-content">
+                                <article class="post">
+                                    <div class="thumb"><a href="blog-detail.html"><img src="https://via.placeholder.com/80x80" alt=""></a></div>
+                                    <h5><a href="blog-detail.html">Magna arcu</a></h5>
+                                    <span class="cat">Alarm</span>
+                                    <div class="text">Vitae est et arcu semper, moll</div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--Footer Column-->
+                    <div class="footer-column col-lg-4 col-md-6 col-sm-12">
+                        <div class="footer-widget contact-widget">
+                            <h4 class="widget-title">OUR CONTACTS</h4>
+                            <div class="widget-content">
+                                <ul class="contact-list">
+                                    <li>
+                                        <i class="far fa-envelope"></i>
+                                        <a href="mialto:Ingenious@gmail.com">Ingenious@gmail.com</a>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-phone"></i> 
+                                        <a href="#">(+123) 456 - 7890</a>
+                                    </li>
+                                    <li>
+                                       <div class="social-icon">
+                                           <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                           <a href="#"><i class="fab fa-twitter"></i></a>
+                                           <a href="#"><i class="fab fa-instagram"></i></a>
+                                           <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                           <a href="#"><i class="fa fa-signal"></i></a>
+                                       </div> 
+                                    </li>
+                                </ul>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--Footer Bottom-->
+        <div class="footer-bottom">
+            <div class="auto-container">
+                <div class="copyright-text">
+                    <p><a href="index.html">Ingenious</a> - Smart Home Automation WordPress Theme</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Main Footer -->
+
+</div><!-- End Page Wrapper -->
+
+<!-- Scroll To Top -->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-long-arrow-alt-up"></span></div>
+<script src="js/jquery.js"></script> 
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--Revolution Slider-->
+<script src="plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="js/main-slider-script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.6/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.6/dist/js/uikit-icons.min.js"></script>
+<!--Revolution Slider-->
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
