4f922d6a5ca6b316e77bd609d273763450a3d03d
Inaya <anwar@anwars-MacBook-Pro.local>
2020-08-27 12:32:11 +0530

    changes

---
+++ resources/views/layouts/welcome_nav.blade.php
@@ -0,0 +1,75 @@
+    <!-- Main Header-->
+    <header class="main-header">
+        <!-- Main box -->
+        <div class="main-box">
+            <div class="menu-box">
+                <div class="logo animate__animated animate__rotateIn animate__delay-1s">
+                    <a href="/">
+                        <img src="images/logo.png" alt="" title="" srcset="images/logo.png 2x">
+                    </a>
+                </div>
+                <!--Nav Box-->
+                <div class="nav-outer">
+                    <!-- Main Menu -->
+                    <nav class="main-menu navbar-expand-md navbar-light">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navigation clearfix">
+                                <li>
+                                    <a href="/"><span class="animate__animated animate__rotateIn animate__delay-1s">Home</span></a>
+                                </li>
+                                <li class="dropdown animate__animated animate__rotateIn animate__delay-1s">
+                                    <a href=""><span class="">Services</span></a>
+                                    <ul>
+                                        <li><a href="home_services">Home Services</a></li>
+                                        <li><a href="office_services">Office Services</a></li>
+                                        <li>helllo</li>
+                                    </ul>
+                                </li>
+                                <li><a href="about" class="animate__animated animate__rotateIn animate__delay-1s"><span>About</span> </a></li>
+                                <li><a href="correre" class="animate__animated animate__rotateIn animate__delay-1s">Careers </a></li>
+                                <li><a href="news" class="animate__animated animate__rotateIn animate__delay-1s" >News </a></li>
+                                <li><a href="contact" class="animate__animated animate__rotateIn animate__delay-1s">Contacts Us</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sticky Header  -->
+        <div class="sticky-header">
+            <div class="outer-box">
+                <!--Logo-->
+                <div class="logo">
+                    <a href="#" title="Brin Tech Logo"><img src="images/logo.png" alt="Brin Tech Logo"></a>
+                </div>
+
+                <!--Nav Outer-->
+                <div class="nav-outer">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <!--Keep This Empty / Menu will come through Javascript-->
+                    </nav><!-- Main Menu End-->
+                </div>
+            </div>
+        </div><!-- End Sticky Menu -->
+
+        <!-- Mobile Header -->
+        <div class="mobile-header">
+            <div class="logo"><a href="/"><img src="images/logo.png" alt="" title="" srcset="images/logo.png 2x"></a></div>
+
+            <!--Nav Box-->
+            <div class="nav-outer clearfix">
+                <!--Keep This Empty / Menu will come through Javascript-->
+            </div>
+        </div>
+
+        <!-- Mobile Menu  -->
+        <div class="mobile-menu">            
+            <nav class="menu-box">
+                <div class="nav-logo"><a href="/"><img src="images/logo.png" alt="" title="" srcset="images/logo.png 2x"></a></div> 
+                <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+            </nav>
+        </div><!-- End Mobile Menu -->
+    </header>
+    <!--End Main Header -->
