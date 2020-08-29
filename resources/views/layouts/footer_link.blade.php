7796e8219c8465d4709a669343ada8c014a36927
Inaya <anwar@anwars-MacBook-Pro.local>
2020-08-29 16:59:12 +0530

    animation in footer

--- resources/views/layouts/footer_link.blade.php
+++ resources/views/layouts/footer_link.blade.php
@@ -4,10 +4,10 @@
         <div class="widgets-section">
             <div class="row">
                 <!--Footer Column-->
-                <div class="footer-column col-lg-4 col-md-6 col-sm-12">
+                <div class="footer-column col-lg-4 col-md-6 col-sm-12" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
                     <div class="footer-widget about-widget">
                         <h4 class="widget-title">BRIN Tech Solutions</h4>
-                        <div class="widget-content" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
+                        <div class="widget-content" >
                             <div class="text">  
                                 We are here to ensure a complete network setup solution that is well suited and beneficial to your organization.</div>
                         </div>
