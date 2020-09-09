43a000b2b0b4fd9f968fd4ee88503f374ff45c99
anwar-hk <anwar.ursource@gmail.com>
2020-09-09 15:12:39 +0530

    changes in css

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -305,21 +305,8 @@
 <!--End Reservation Section -->
 
 <!--Clients Section-->
-<section class="clients-section">
-    <div class="auto-container">
-        <!--Sponsors carousel-->
-        <ul class="sponsors-carousel owl-carousel owl-theme">
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-            <li class="slide-item"><figure class="image-box"><a href="#"><img src="https://via.placeholder.com/100x100" alt=""></a></figure></li>
-        </ul>
-    </div>
-</section>
+@include('layouts.our_partner')
+
 <!--End Clients Section-->
 
 @include('layouts.footer_link')
